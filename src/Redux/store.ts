14af2a5ab833084fdf05fdcb4e67bac97c25f023
RohitSignIn <rohitsinghsignin@gmail.com>
2023-10-25 19:23:46 +0530

    removed getDefault Middleware

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,12 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import ForecastReducer from './Slices/ForecastSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import ForecastReducer from "./Slices/ForecastSlice";
 const store = configureStore({
-    reducer: {
-        forecast: ForecastReducer
-    },
-    devTools: true,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false})
+  reducer: {
+    forecast: ForecastReducer,
+  },
+  devTools: true,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export type AppDispatch = typeof store.dispatch;
