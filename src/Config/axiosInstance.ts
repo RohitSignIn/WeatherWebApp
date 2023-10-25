14ae672cb6e5960bad4348995150b8c86654bf7f
RohitSignIn <rohitsinghsignin@gmail.com>
2023-10-25 19:37:31 +0530

    Change http to secure https api request

--- src/Config/axiosInstance.ts
+++ src/Config/axiosInstance.ts
@@ -1,10 +1,10 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance } from "axios";
 
-const instance : AxiosInstance = axios.create({
-    baseURL: `http://api.weatherapi.com/v1`,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+const instance: AxiosInstance = axios.create({
+  baseURL: `https://api.weatherapi.com/v1`,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default instance;
